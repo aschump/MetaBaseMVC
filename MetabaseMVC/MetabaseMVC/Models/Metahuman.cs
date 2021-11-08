ce85ae025b85f9e230d85966c313c5488555f029
schumpertamber@gmail.com <schumpertamber@gmail.com>
2021-11-08 12:32:03 -0500

    Added Metahuman, Skill, Species models. Added MetahumanController file

---
+++ MetabaseMVC/MetabaseMVC/Models/Metahuman.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MetabaseMVC.Models
+{
+    public class Metahuman
+    {
+        [Key]
+        public int MetaID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [ForeignKey(nameof(Skill))]
+        public int SkillsID { get; set; }
+        [Required]
+        [ForeignKey(nameof(Species))]
+        public int SpeciesID { get; set; }
+        [Required]
+        public string Bio { get; set; }
+    }
+}
