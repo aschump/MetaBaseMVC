c8b8fcf17f671dc52868204abcfdeef00d361b65
schumpertamber@gmail.com <schumpertamber@gmail.com>
2021-11-13 19:29:46 -0500

    Added data models, metahumancontroller, and new view to metahuman folder

---
+++ MetabaseMVC/MetabaseMVC/Models/Metahuman.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MetabaseMVC.Models
+{
+    public class Metahuman
+    {
+        [Key]
+        public int MetaID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int SkillsID { get; set; }
+        [Required]
+        public int SpeciesID { get; set; }
+        [Required]
+        public string Bio { get; set; }
+    }
+}
